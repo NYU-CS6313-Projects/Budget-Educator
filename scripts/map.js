8aad6dedf0b850e5289dcd8cca833ef84157fc73
Aarti Patel <akpatel.120@gmail.com>
2015-05-08 23:34:25 -0400

    playing with layers

--- scripts/map.js
+++ scripts/map.js
@@ -1,6 +1,27 @@
 var url = 'https://aartyparty.cartodb.com/api/v2/viz/2ab32f3e-ede5-11e4-a96e-0e43f3deba5a/viz.json';
+var map;
 
 cartodb.createVis('map', url)
 	.done(function(vis, layers) {
-		// var map = vis.getNativeMap();
+		map = vis.getNativeMap();
+
+		console.log(layers);
+
+			// Add custom on-click functionality to map
+			// cartodb.createLayer(map, url)
+			// .addTo(map).on('done', function(layer) {	
+				
+			// 	var subLayerOptions = {
+			// 		cartocss: "#overview{marker-fill: #fff;}"
+			// 	}
+
+			// 	var sublayer = layer.getSubLayer(0);
+			// 	sublayer.set(subLayerOptions);
+			    
+			// 	sublayer.on('featureClick', function(e, latlng, pos, data) {
+			// 		console.log(data);
+			// 	});
+		 //  	});
+
 	});
+
