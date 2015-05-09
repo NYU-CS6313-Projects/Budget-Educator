2b83fab8885a11452240257268088d4ed8e0a41f
Aarti Patel <akpatel.120@gmail.com>
2015-05-09 01:01:13 -0400

    map changes based off selection in table

--- scripts/map.js
+++ scripts/map.js
@@ -1,27 +1,25 @@
 var url = 'https://aartyparty.cartodb.com/api/v2/viz/2ab32f3e-ede5-11e4-a96e-0e43f3deba5a/viz.json';
 var map;
+var mapLayerWithAllSchools;
+var arrSelectedSchools = new Array();
 
 cartodb.createVis('map', url)
 	.done(function(vis, layers) {
 		map = vis.getNativeMap();
-
-		console.log(layers);
-
-			// Add custom on-click functionality to map
-			// cartodb.createLayer(map, url)
-			// .addTo(map).on('done', function(layer) {	
-				
-			// 	var subLayerOptions = {
-			// 		cartocss: "#overview{marker-fill: #fff;}"
-			// 	}
-
-			// 	var sublayer = layer.getSubLayer(0);
-			// 	sublayer.set(subLayerOptions);
-			    
-			// 	sublayer.on('featureClick', function(e, latlng, pos, data) {
-			// 		console.log(data);
-			// 	});
-		 //  	});
-
+		mapLayerWithAllSchools = layers[1].getSubLayer(0);	
 	});
 
+var updateMap = function(){
+
+	// console.log("SELECT * FROM overview WHERE dbn IN (" + arrSelectedSchools + ")");
+
+	mapLayerWithAllSchools.setSQL("SELECT * FROM overview WHERE dbn IN (" + arrSelectedSchools + ")"); 
+}
+
+var updateSelectedSchoolsFromTable = function(){
+	arrSelectedSchools = new Array();
+	$(".schoolSelected").each(function(){
+		arrSelectedSchools.push("'" + $(this)[0].id + "'");
+	});
+	updateMap();
+}
--- scripts/table.js
+++ scripts/table.js
@@ -32,6 +32,7 @@ var tabulate = function (data, columns) {
 	  .on( 'click', function() {
 	  	// If you click on the row, it adds or removes the .schoolSelected class
 		  $( this ).toggleClass( 'schoolSelected' );
+		  updateSelectedSchoolsFromTable();
 		});
 
 	// Adds the individual cells per row
